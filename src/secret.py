84625629148f88c36c5f24890aa25e987882698a
Jasper O'Leary <jhaazpr@gmail.com>
2016-01-26 20:59:49 -0800

    actually add classes, separate src/ and data/

---
+++ src/secret.py
@@ -0,0 +1,7 @@
+# DO NOT PUSH TO PUBLIC REPOSITORY
+''' Keys for authenticating with the application '''
+
+consumer_key = '7k9B6QeLyqvjWzjtAWWf1gFXd'
+consumer_secret = 'zOIxkJUFChgnlN6UqiU6nIIChcNq5elyKgDOuT6ivS6UjXuHUZ'
+access_token_key = '3667348392-36nYUMoFVL0BoG4PlNNMDOAMq7i6LyuMjahpNC3'
+access_token_secret = 'L1A7TLWQLRizAXr57wIJ4SOng4snh00xEh3BOFyipxW7d'
