15f2c3a6e2acd746d0a07546176e8746328a2555
Jasper O'Leary <jhaazpr@gmail.com>
2016-05-25 18:18:36 -0700

    tweaks to live wire code, inclusing startup pulse behavior

--- src/MotorCode/MotorCode.ino
+++ src/MotorCode/MotorCode.ino
@@ -5,13 +5,19 @@
 #include <AFMotor.h>
 
 AF_DCMotor motor(4);
-int incomingByte = 0;  
+int incomingByte = 0;
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
    motor.run(RELEASE);
+
+   // Quickly start and stop to know the motor is on
+   motor.run(FORWARD);
+   motor.setSpeed(255);
+   delay(500);
+   motor.run(RELEASE);
 }
 void loop() {
-  Serial.println("I am on");
+  Serial.println("I am the motorshield.");
   if (Serial.available() > 0) {
       // read the incoming byte:
       incomingByte = Serial.read();
@@ -22,16 +28,20 @@ void loop() {
         
       }
       else if(incomingByte == 1 || incomingByte == 49){
-         motor.run(FORWARD);
-        motor.setSpeed(200);
+        motor.run(FORWARD);
+        motor.setSpeed(110);
       }
       else if(incomingByte == 2 || incomingByte == 50){
-         motor.run(FORWARD);
-        motor.setSpeed(220);
+        motor.run(FORWARD);
+        motor.setSpeed(160);
       }
       else if(incomingByte == 3 || incomingByte == 51){
-         motor.run(FORWARD);
-        motor.setSpeed(240);
+        motor.run(FORWARD);
+        motor.setSpeed(210);
+      }
+      else if(incomingByte == 4 || incomingByte == 52){
+        motor.run(FORWARD);
+        motor.setSpeed(255);
       }
    }
    delay(500);              // wait for a second
